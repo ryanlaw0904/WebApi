24e1fe731bd867a2783848cc530e71930c395a17
ryanlaw0904 <134366807+ryanlaw0904@users.noreply.github.com>
2025-02-27 21:21:27 +0800

    Add files via upload

--- articles.ts
+++ articles.ts
@@ -1,74 +1,31 @@
-import Router, {RouterContext} from 'koa-router';
-import bodyParser from 'koa-bodyparser';
-import { body } from 'koa-req-validation';
-import * as model from '../models/articles';
-
-const router = new Router({prefix: '/api/v1/articles'});
-// const articles = [
-//     {title: 'hello article', fullText: 'some text here'},
-//     {title: 'another article', fullText: 'another text'},
-//     {title: 'Coventry University', fullText:'some news from university'}
-// ];
-
-interface Article {
-    title: string,
-    fullText: string
-}
-
-const getAll = async (ctx:RouterContext, next: any) => {
-    ctx.body = articles;
-    await next();
-}
-
-const getById = async (ctx:RouterContext, next: any) => {
-    let id = +ctx.params.id;
-    if((id<=articles.length) && id>0){
-        ctx.body = articles[id-1];
-    } else {
-        ctx.status = 404;
+import * as db from '../helpers/database';
+//get a single article by its i
+export const getById = async (id: any) => {
+    let query = "SELECT * FROM articles WHERE ID = ?"
+    let values = [id]
+    let data = await db.run_query(query, values);
+    return data;
+   }
+   //list all the articles in the database
+   export const getAll = async () => {
+    // TODO: use page, limit, order to give pagination
+    let query = "SELECT * FROM articles;"
+    let data = await db.run_query(query, null);
+    return data;
+   }
+   //create a new article in the database
+   export const add = async (article: any) => {
+    let keys = Object.keys(article);
+    let values = Object.values(article);
+    let key = keys.join(',');
+    let param = '';
+    for(let i: number=0; i<values.length; i++){ param +='?,'}
+    param=param.slice(0,-1);
+    let query = `INSERT INTO articles (${key}) VALUES (${param})`;
+    try{
+    await db.run_insert(query, values);
+    return {status: 201};
+    } catch(err: any) {
+    return err;
     }
-    await next();
-}
-const createArticle = async (ctx:RouterContext, next: any) => {
-    let article = <Article> ctx.request.body;
-    let newArticle = {title: article.title, fullText: article.fullText};
-    articles.push(newArticle);
-    ctx.status = 201;
-    ctx.body = newArticle;
-    await next();
-}
-const updateArticle = async (ctx:RouterContext, next: any) => {
-    let id = +ctx.params.id;
-    let article = <Article> ctx.request.body;
-    let updateArticle = {title: article.title, fullText: article.fullText};
-    if((id<=articles.length) && id>0){
-        articles[id-1].title = updateArticle.title;
-        articles[id-1].fullText = updateArticle.fullText;
-        ctx.body = {
-            message: "update successfully"
-        }
-    } else {
-        ctx.status = 404;
-    }
-    await next();
-}
-const deleteArticle = async (ctx:RouterContext, next: any) => {
-    let id = +ctx.params.id;
-    if((id<=articles.length) && id>0){
-        articles.splice(id-1, 1);
-        ctx.body = {
-            message: "Article deleted"
-        }
-    } else {
-        ctx.status = 404;
-    }
-    await next();
-}
-
-router.get('/', getAll);
-router.get('/:id([0-9]{1,})', getById);
-router.post('/', bodyParser(), createArticle);
-router.put('/:id([0-9]{1,})', bodyParser(), updateArticle);
-router.delete('/:id([0-9]{1,})', deleteArticle);
-
-export { router };
+   }
